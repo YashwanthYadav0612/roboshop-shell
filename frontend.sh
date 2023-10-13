b17dca9cedd133460194d107d85ecf4c5b995131
Yash Duvvi <yashwanthyadav0612@gmail.com>
2023-10-13 13:43:13 +0530

    Roboshop nginx script

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-vim /etc/nginx/default.d/roboshop.conf
+cp /root/roboshop-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx
 systemctl start nginx
